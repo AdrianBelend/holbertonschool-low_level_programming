9de2b3aa63a188046b63808342d4256bc6c752df
adrianbelend <5730@holbertonstudents.com>
2022-10-03 09:05:18 -0700

    5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - Entry point
+ *
+ * Return: 0 , 1
+ * @c: int
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(0);
+	}
+	return (0);
+}
