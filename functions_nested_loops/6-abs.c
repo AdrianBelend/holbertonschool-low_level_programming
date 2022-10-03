99d17f2133fc608705565db40da653d348009be3
adrianbelend <5730@holbertonstudents.com>
2022-10-03 10:11:04 -0700

    6

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -7,13 +7,12 @@
  */
 int _abs(int n)
 {
-	if (n >= 0)
+	if (n > 0)
 	{
-		_putchar(n - '0');
+		return (n);
 	}
-	else if (n < 0)
+	else
 	{
-		_putchar((n * -1) - '0');
+		return (n * -1);
 	}
-	return (0);
 }
