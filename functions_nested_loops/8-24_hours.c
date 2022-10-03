8826f88a58d8bae3c848d1471bc725474d034ac7
adrianbelend <5730@holbertonstudents.com>
2022-10-03 12:30:37 -0700

    8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * jack_bauer - Entry point
+ *
+ */
+void jack_bauer(void)
+{
+	int hour = 0;
+	int min = 0;
+
+	for (hour = 0 ; hour <= 23 ; hour++)
+	{
+		_putchar((hour / 10) + '0');
+		_putchar((hour % 10) + '0');
+		_putchar(':');
+		_putchar((min / 10) + '0');
+		_putchar((min % 10) + '0');
+		_putchar('\n');
+
+	for (min = 0 ; min <= 59 ; min++)
+	{
+		_putchar((hour / 10) + '0');
+		_putchar((hour % 10) + '0');
+		_putchar(':');
+		_putchar((min / 10) + '0');
+		_putchar((min % 10) + '0');
+		_putchar('\n');
+	}
+	}
+
+}
