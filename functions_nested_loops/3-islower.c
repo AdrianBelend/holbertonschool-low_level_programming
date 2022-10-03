7b023872d2decc6f01f8aa58f0d0e280ec799e97
adrianbelend <5730@holbertonstudents.com>
2022-10-03 06:44:39 -0700

    3

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -3,12 +3,13 @@
  * _islower - Entry point
  *
  * Return: 0 , 1
+ * @c: int
  */
 int _islower(int c)
-{	
+{
 	if (c >= 'a' && c <= 'z')
 	{
-		return(1);
+		return (1);
 	}
-	return(0);	
+	return (0);
 }
