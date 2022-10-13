ebf097031b118e6facb4cc394a2a33a135cd28da
adrianbelend <5730@holbertonstudents.com>
2022-10-13 09:53:58 -0700

    5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rev_string - Entry level
+ * @s: char *
+ */
+
+void rev_string(char *s)
+{
+	int len = 0;
+	int i;
+	char tmp = s[0];
+
+	for (; s[len] != '\0' ; len++)
+	{
+	}
+	for (i= 0; i < len; i++)
+	{
+		len--;
+		tmp = s[i];
+		s[i] = s[len];
+		s[len] = tmp;
+	}
+}
