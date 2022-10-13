57b47cb8e259866e8a587ce5590cfcc4edc67005
adrianbelend <5730@holbertonstudents.com>
2022-10-13 10:05:06 -0700

    9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *_strcpy - Entry level
+ * @dest: char *
+ * @src: char *
+ * Return: 0
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int a;
+
+	for (; src[a] != '\0'; a++)
+	{
+		dest[a] = src[a];
+	}
+	return (dest);
+}
