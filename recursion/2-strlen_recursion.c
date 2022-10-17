fd2042394b32c914ca1be81169cfd8afa62338a2
adrianbelend <5730@holbertonstudents.com>
2022-10-17 06:20:51 -0700

    2

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -2,13 +2,14 @@
 /**
  * _strlen_recursion - Entry point
  * @s: char *
+ * Return: 0
  */
 
 int _strlen_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		return( 1 + _strlen_recursion(s + 1));
+		return (1 + _strlen_recursion(s + 1));
 	}
-	return(0);
+	return (0);
 }
