bc4251bad9be1ad0ae9ffc8ed741066ab34e70be
adrianbelend <5730@holbertonstudents.com>
2022-10-27 16:39:42 -0700

    45

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -12,11 +12,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	struct dog *p;
 
-	p=malloc(sizeof(struct dog));
+	p = malloc(sizeof(struct dog));
 
 	p->age = age;
 	p->name = name;
 	p->owner = owner;
-	
 	return (p);
 }
