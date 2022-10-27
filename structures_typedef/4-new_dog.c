9c30248918c545fb41a1fbc89903d9e3e6ffc4ff
adrianbelend <5730@holbertonstudents.com>
2022-10-27 16:37:01 -0700

    45

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * new_dog - Entry point
+ * @name: char *
+ * @age: float
+ * @owner: char*
+ * Return: 0
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog *p;
+
+	p=malloc(sizeof(struct dog));
+
+	p->age = age;
+	p->name = name;
+	p->owner = owner;
+	
+	return (p);
+}
