97ffd1019955bedbd5e2ba76c4b2f7bd2a6f37e5
adrianbelend <5730@holbertonstudents.com>
2022-10-31 19:15:54 -0700

    1

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+ *array_iterator - function
+ *@array: int *
+ *@size: size_t
+ *@action: pointer to a function
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+
+	int i;
+
+	if (array == NULL || action == NULL)
+	{
+		return;
+	}
+	for (i = 0; size > 0; i++)
+	{
+		action(array[i]);
+		size--;
+	}
+}
