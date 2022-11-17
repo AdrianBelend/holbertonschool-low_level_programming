a10d2970dd22c761b00c007eff14015f2418c3f0
adrianbelend <5730@holbertonstudents.com>
2022-11-17 05:24:33 -0800

    task0

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_lists - Entry point
+ * @h: pointer
+ * Return: 0
+ */
+size_t print_list(const list_t *h)
+{
+	int c;
+
+	if (h == NULL)
+		return (0);
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[%d] (nil)\n", 0);
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		c++;
+	}
+	return (c);
+}
