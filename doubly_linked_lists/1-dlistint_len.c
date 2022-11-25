1f6f507c35deb92f5d6e6c560812b795ced5d173
adrianbelend <5730@holbertonstudents.com>
2022-11-25 05:53:33 -0800

    1

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * listint_len - Entry point
+ * @h: pointer
+ * Return: 0
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int c = 0;
+
+	if (h == NULL)
+		return (0);
+	while (h != NULL)
+	{
+		h = h->next;
+		c++;
+	}
+	return (c);
+}
